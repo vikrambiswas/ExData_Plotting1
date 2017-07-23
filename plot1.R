e848e94ddd213956ed386679009d968df0d0b518
vikrambiswas <vikramstl@gmail.com>
2017-07-23 15:07:43 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,3 +8,4 @@ png("plot1.png", width=480, height=480)
 hist(powerDT[, Global_active_power], main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.off()
 
+# plot1.png is in 
