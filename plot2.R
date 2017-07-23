7e8b81658726d64c5b7988480e2c88e7e1d4c9bc
vikrambiswas <vikramstl@gmail.com>
2017-07-23 14:58:23 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,9 @@
+library("data.table")
+setwd( "C:/Users/Administrator/Documents")
+powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot2.png", width=480, height=480)
+plot(x = powerDT[, dateTime], y = powerDT[, Global_active_power], type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
