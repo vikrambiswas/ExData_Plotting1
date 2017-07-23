595e683c26ebdcff711fa6db100773e7afb76c18
vikrambiswas <vikramstl@gmail.com>
2017-07-23 15:09:06 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,3 +13,5 @@ legend("topright"
        , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
        ,lty=c(1,1), lwd=c(1,1))
 dev.off()
+
+# plot3.png is in folder "figure" as file "unnamed-chunk-4.png"
